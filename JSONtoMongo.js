11534c90e5b80087a5d0588d6d39a7093497797b
nellafeht06 <nellafeht06@ufl.edu>
2016-01-21 00:02:07 -0500

    Create JSONtoMongo.js

---
+++ JSONtoMongo.js
@@ -0,0 +1,33 @@
+'use strict';
+/* 
+  Import modules/files you may need to correctly run the script. 
+  Make sure to save your DB's uri in the config file, then import it with a require statement!
+ */
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'), 
+	listings = require('./listings.json'),
+    config = require('./config');
+
+/* Connect to your database */
+	mongoose.connect(config.db.uri);
+/* 
+  Instantiate a mongoose model for each listing object in the JSON file, 
+  and then save it to your Mongo database 
+*/
+for(var i=0;i<listings.entries.length;i++){
+	var dataFromListings= new Listing({
+		code: listings.entries[i].code,
+		name: listings.entries[i].name,
+		coordinates: listings.entries[i].coordinates,
+		address: listings.entries[i].address
+	});
+	dataFromListings.save(function(err){
+		if(err) throw err;
+	});
+};
+/* 
+  Once you've written + run the script, check out your MongoLab database to ensure that 
+  it saved everything correctly.
+ */
